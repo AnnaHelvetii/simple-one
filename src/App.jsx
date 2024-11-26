cd0b734c5b13d3525f63ee8ddd4442b005ea4ec7
Anna Soboleva <sithhappens1138@gmail.com>
2024-11-26 12:44:51 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/main.scss';
+
+function App() {
+	return (
+		<div className="App">
+			<p className='p1'>Тестовый текст!!!</p>
+			<p className='p2'>Тестовый текст!!!</p>
+		</div>
+	);
+}
+
+export default App;
